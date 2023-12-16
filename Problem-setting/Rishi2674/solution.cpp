f560b4b3ec69e6ae630b1f7c1a226ecc5b87f473
Fremder <118520625+Fremdder@users.noreply.github.com>
2023-12-16 11:50:45 +0530

    Update solution.cpp
    changed soultion.cpp to have better solution according to question statment

--- Problem-setting/Rishi2674/solution.cpp
+++ Problem-setting/Rishi2674/solution.cpp
@@ -10,7 +10,9 @@ int main(){
         float max_dist = a/(1-(1.0/x));
         if(max_dist>=n) cout<<"YES\n";
         else{
-            cout<<"NO"<<" "<<round(n-max_dist)<<endl;
+            //cout<<"NO"<<" "<<round(n-max_dist)<<endl;
+            //you can use this instead for getting accurate answer which is correct according to question statment 
+            cout << "NO" << " " << n-max_dist << endl;
         }
     }
     return 0;
