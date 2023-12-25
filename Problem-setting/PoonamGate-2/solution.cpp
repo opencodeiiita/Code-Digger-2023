174ed8c13f0ffd8ad5a775dd5615af508f025eaa
PoonamGate <120942108+PoonamGate@users.noreply.github.com>
2023-12-25 10:26:18 +0530

    Update solution.cpp

--- Problem-setting/PoonamGate-2/solution.cpp
+++ Problem-setting/PoonamGate-2/solution.cpp
@@ -1,7 +1,17 @@
-
 #include <iostream>
 #include<cmath>
 using namespace std;
+
+const int mod = 1e9 + 7;
+
+int power(int a, int p) {
+    if(p == 0) return 1;
+    if(p % 2 == 1) {
+        return (a * 1ll * power(a, p - 1)) % mod;
+    }
+    int half = power(a, p / 2);
+    return (half * 1ll * half) % mod;
+}
 
 int main()
 {
@@ -15,13 +25,13 @@ int main()
     while(q--){
         int A,l,r;
         cin>>A>>l>>r;
-        a[l]+=A;
-        a[r+1]+=(-A*pow(k,r-l+1));
-      
-        
+        a[l] += A % mod;
+        int num = (A * 1ll * power(k, r - l + 1)) % mod;
+        int negative_modulo = (mod - num) % mod;
+        a[r+1] += negative_modulo;        
     }
     for(int i=2;i<=n;++i){
-        a[i]=(a[i]+(a[i-1]*k));
+        a[i] = (a[i] + (a[i-1] * 1ll * k) % mod) % mod ;
         
     }
     
@@ -33,4 +43,5 @@ int main()
     return 0;
 }
 
-(modulus lgana rh gya ...i think ...time nhi h)
+        
+        
