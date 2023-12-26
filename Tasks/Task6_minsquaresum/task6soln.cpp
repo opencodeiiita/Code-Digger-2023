e138bf6de3c8eaf77e80b54cefd1e3ca3a41801f
PoonamGate <120942108+PoonamGate@users.noreply.github.com>
2023-12-26 22:57:08 +0530

    Update task6soln.cpp

--- Tasks/Task6_minsquaresum/task6soln.cpp
+++ Tasks/Task6_minsquaresum/task6soln.cpp
@@ -5,13 +5,14 @@ int n;
 int dp[10010];
 int rec(int level){
       if(level==0) return 0;
+      if(level==1) return 1;
      if(level==2) return 2;
       if(level==3) return 3;
       if(dp[level]!=-1) return dp[level];
       int ms=INT_MAX;
       int r=sqrt(level);
      
-   for(int i=1;i<=r;++i){
+   for(int i=2;i<=r;++i){
       if(level-i*i>=0){
           ms=min(ms,1+rec(level-i*i));
       }
